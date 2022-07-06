1dac03785876f75c9cddb163c84d2fca93de773b
costamay <titusouko@gmail.com>
2022-07-06 20:27:40 +0300

    commit tree 98457f5b3d20220209cdcda9791deba6b28a8b2d

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,8 +8,13 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    plant = find_plant
+    if plant
+      render json: plant
+    else
+      render_not_found_response
+    end
+    
   end
 
   # POST /plants
@@ -18,9 +23,37 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+  def destroy
+    plant = find_plant
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render_not_found_response
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
   end
 end
